8136171bdf5e67e4f48027822ef32acb3e95dda2
Christopher <ch.welschoff@googlemail.com>
2026-08-24 12:41:30 +0200

    add spotlight color

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+
 export default function HomePage() {
+  const URL = "https://example-apis.vercel.app/api/art";
+
+  const [art, setArt] = useState();
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [spotlightArt, setSpotlightArt] = useState(art);
+
+  useEffect(() => {
+    async function fetchArt() {
+      setisLoading(true);
+      try {
+        const response = await fetch(URL);
+
+        if (!response.ok) {
+          throw new Error("HTTP-Fehler");
+        }
+        const data = await response.json();
+        setArt(data);
+        setSpotlightArt(getRandomArt(data));
+      } catch (error) {
+        setError(true);
+      }
+      setisLoading(false);
+    }
+    fetchArt();
+  }, []);
+
+  function getRandomArt(artArray) {
+    const randomArtIndex = Math.floor(Math.random() * artArray.length);
+    return artArray[randomArtIndex];
+  }
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <p>{spotlightArt.artist}</p>
+          <Image
+            src={spotlightArt.imageSource}
+            width={spotlightArt.dimensions.width}
+            height={spotlightArt.dimensions.height}
+          />
+        </div>
+      )}
+      <p>{error ? "Error fetching data..." : ""}</p>
     </div>
   );
 }
+
+
