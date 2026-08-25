7fb0f78281e6c0a008b6852fc0515af8659bcae9
Christopher <ch.welschoff@googlemail.com>
2026-08-25 16:25:13 +0200

    style comment section

--- components/CommentSection/CommentSection.jsx
+++ components/CommentSection/CommentSection.jsx
@@ -1,4 +1,5 @@
 import Comment from "../Comment/Comment";
+import styled from "styled-components";
 
 export default function CommentSection({ comments, slug }) {
   const pieceComments = comments.find(
@@ -8,19 +9,57 @@ export default function CommentSection({ comments, slug }) {
   console.log(pieceComments);
 
   return (
-    <section>
-      <h3>Comments:</h3>
-      <ul>
+    <Section>
+      <Title>Comments:</Title>
+      <CommentList>
         {pieceComments ? (
           pieceComments.map((comment) => (
-            <li key={comment.date}>
+            <CommentItem key={comment.date}>
               <Comment text={comment.text} date={comment.date} />
-            </li>
+            </CommentItem>
           ))
         ) : (
-          <p>No comments so far...</p>
+          <EmptyMessage>No comments so far...</EmptyMessage>
         )}
-      </ul>
-    </section>
+      </CommentList>
+    </Section>
   );
 }
+
+const Section = styled.section`
+  width: 100%;
+  max-width: 520px;
+  margin: 30px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h3`
+  margin: 0 0 20px;
+  text-align: center;
+`;
+
+const CommentList = styled.ul`
+  width: 100%;
+  margin: 0;
+  padding: 0;
+
+  list-style: none;
+
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const CommentItem = styled.li`
+  width: 100%;
+`;
+
+const EmptyMessage = styled.p`
+  text-align: center;
+  color: #999;
+`;
