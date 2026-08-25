552dab2e6f99b6221599378f17e9ecabbc353f8a
sebastian <plxcxs@protonmail.com>
2026-08-25 10:12:17 +0200

    refactored

--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-const URL = "https://example-apis.vercel.app/api/art";
 
-export default function ArtPieceDetail({ piece }) {
+export default function ArtPieceDetail({ art }) {
   const router = useRouter();
+  const { slug } = router.query;
+  const piece = art.find((p) => p.slug === slug);
+  if (!piece) return <p>Loading ...</p>;
   return (
     <div>
       <h1>{piece.name}</h1>
@@ -19,32 +21,4 @@ export default function ArtPieceDetail({ piece }) {
       <button onClick={() => router.push("/art-pieces")}>Back</button>
     </div>
   );
-}
-
-async function fetchAllArt() {
-  const response = await fetch(URL);
-  return await response.json();
-}
-
-export async function getStaticPaths() {
-  const art = await fetchAllArt();
-
-  const paths = art.map((piece) => ({
-    params: { slug: piece.slug },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const art = await fetchAllArt();
-
-  const piece = art.find((p) => p.slug === params.slug);
-
-  return {
-    props: { piece },
-  };
 }
